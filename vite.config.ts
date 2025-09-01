ed162e1646ce90252fbfd95c5e8002c8989e58e8
pappan76 <54273114+pappan76@users.noreply.github.com>
2025-09-01 17:56:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "CoreRunners", // 👈 replace with your repo name
+  base: "/CoreRunners/"  // 👈 must match your repo name
 });
