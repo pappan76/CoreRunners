f6e6686e7197374e5654d5ada51fbaddc3d39ee3
pappan76 <54273114+pappan76@users.noreply.github.com>
2025-09-01 23:04:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   base: "/CoreRunners/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
+      "@": path.resolve(__dirname, "./src")
     }
   }
 });
