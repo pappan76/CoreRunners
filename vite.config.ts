4403d5fe7958056f1085ca76671140866b70acf1
pappan76 <54273114+pappan76@users.noreply.github.com>
2025-09-01 18:04:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CoreRunners/"  // 👈 must match your repo name
+  base: "/CoreRunners/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 });
