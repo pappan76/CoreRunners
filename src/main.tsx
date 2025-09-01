a53b20b3599fe47e96718da42f9ccbe6dd74dcd8
pappan76 <54273114+pappan76@users.noreply.github.com>
2025-09-01 18:17:30 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global.css";
 
+// Import Firebase config (must export a module)
+import "./firebase";
+
+// Render the app
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
