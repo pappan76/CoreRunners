b86271b7d3759748a7d117d1c4eb8e0f3e0fa505
pappan76 <54273114+pappan76@users.noreply.github.com>
2025-09-01 22:40:14 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "/App";
+import App from "./App";
 import "/global.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
@@ -13,5 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
+
 
 
